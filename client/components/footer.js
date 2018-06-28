8cebbf68dde678da7b28e0557c76a277e48adf6c
Jimmi Chen <gchen20@u.rochester.edu>
2018-06-27 21:16:24 -0400

    add footer

---
+++ client/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            Login page made by <em><a href="mailto:gchen20@u.rochester.edu">me</a></em>
+        </footer>
+    )
+}
+
+export default Footer
